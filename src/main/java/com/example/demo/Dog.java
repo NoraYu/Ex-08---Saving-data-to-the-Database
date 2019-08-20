a235c779ca54683b6590b501417a30f2610561cd
NoraYu <yxy_1990@hotmail.com>
2019-08-20 15:48:34 -0400

    1

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    @NotNull
+    public String getName() {
+        return name;
+    }
+    @NotNull
+    @Min(1)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
